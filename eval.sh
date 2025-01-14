315d151c8c10c468d8e81c7c216bfe98fa380041
JongbinWoo <root@pggaudi-s013-nicl.nfra.io>
2025-01-14 12:20:06 +0000

    Modify yaml file

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-PT_HPU_LAZY_MODE=0 python train.py --cfg-path configs/paper_final.yaml --options run.do_eval=True model.resume_from=$1
+PT_HPU_LAZY_MODE=0 python train.py --cfg-path configs/paper_final.yaml --options run.do_eval=True model.resume_from=/path/to/ckpt
